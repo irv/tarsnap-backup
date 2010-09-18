b684bb50a04b6ab800ca71c307546486898eb771
Andy Irving <andy@soundforsound.co.uk>
2010-09-18 15:45:40 +0100

    Tidied up lines that were too long. Added license. New option to retain x number of backups.

--- tarsnap-backup.hs
+++ tarsnap-backup.hs
@@ -1,18 +1,32 @@
 {-# LANGUAGE DeriveDataTypeable #-}
-{-
+{--
 
-(c) Andy Irving 2010
-This produces archives in the following format:
+Copyright (c) 2010, Andy Irving
+All rights reserved.
 
-Basename-Frequency-YYYY-MM-DD
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
 
-where Frequency is Daily/Weekly/Monthly
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-If cleanup is not disabled, archives of the next lower type will be deleted.
-So if the Frequency is Monthly, any Daily archives for this Basename are
-deleted.
-
--}
+--}
 
 import Data.Time.Calendar
 import Data.Time.Clock
@@ -28,12 +42,17 @@ data Frequency = Daily | Weekly | Monthly | Auto
     deriving (Read, Show, Eq, Data, Typeable)
 
 doBackup :: String -> Day -> FilePath -> IO ExitCode
-doBackup f d b = readProcessWithExitCode "tarsnap" ["-c", "-f", archive_name b, b] [] >>= checkRes
+doBackup f d b = readProcessWithExitCode
+                    "tarsnap" ["-c", "-f", archive_name b, b] [] >>= checkRes
     where checkRes (rc, out, err) = case rc of
-                                            ExitFailure eno -> error $ "Unable to backup:\n" ++ err
+                                            ExitFailure eno -> error $
+                                                "Unable to backup: (" ++ 
+                                                show eno ++ ")\n" ++ err
                                             ExitSuccess -> return rc
-          basename d =  last (splitDirectories d) -- /home/irv/blah == blah
-          archive_name b = concat $ intersperse "-" [basename b, f, showGregorian d] -- blah-Frequency-2010-09-11
+          basename d =  last (splitDirectories d) -- /path/to/blah == blah
+           -- blah-Frequency-2010-09-11
+          archive_name b = concat $ 
+                            intersperse "-" [basename b, f, showGregorian d]
 
 -- When doing the cleanup, remove backups which are of the lower frequency
 whatCleanup :: Frequency -> Maybe Frequency
@@ -42,40 +61,53 @@ whatCleanup f = whatType f
           whatType Weekly = Just Daily
           whatType Monthly = Just Weekly
 
-doCleanup :: String -> Frequency -> IO ExitCode
-doCleanup b f = readProcessWithExitCode "tarsnap" ["--list-archives"] [] >>= continueCleanup
-    where continueCleanup (rc, out, err) = case rc of
-                                                ExitFailure eno -> error $ "Unable to list archives for cleanup:\n" ++ err
-                                                ExitSuccess -> case (getCleanupList b f (lines out)) of
-                                                                    [] -> return rc
-                                                                    (xs) -> execCleanup xs;
+doCleanup :: String -> Frequency -> Int -> IO ExitCode
+doCleanup b f n = readProcessWithExitCode
+                    "tarsnap" ["--list-archives"] [] >>= goCleanup
+    where goCleanup (rc, out, err) = case rc of
+                                       ExitFailure eno -> error $
+                                         "Unable to list archives for cleanup"
+                                           ++ "\n" ++ err
+                                       ExitSuccess -> case 
+                                         (getCleanupList b f (lines out)) of
+                                           [] -> return rc
+                                           (xs) -> execCleanup
+                                                (drop n (reverse xs))
 
 execCleanup :: [String] -> IO ExitCode
 execCleanup l = do
-    (rc, out, err) <- readProcessWithExitCode "tarsnap" (["-d", "-f"] ++ intersperse "-f" l) []
+    (rc, out, err) <- readProcessWithExitCode
+        "tarsnap" (["-d", "-f"] ++ intersperse "-f" l) []
     case rc of
          ExitFailure eno ->  error err
          ExitSuccess -> return rc
 
--- from the input list ["blah-Frequency-2010-09-11", "blah-Frequency-2010-09-12"]
+-- from the input list:
+-- ["blah-Frequency-2010-09-11", "blah-Frequency-2010-09-12"]
 -- strip the "blah-" string if it's an archive of this dir
 -- then strip the "Frequency-" string if it's the frequency we're clearing up
 -- then remove any Nothing's from the list
 getCleanupList :: String -> Frequency -> [String] -> [String]
-getCleanupList b f l = sort . map ((b ++ "-" ++ show f ++ "-") ++) $ mapMaybe (stripPrefix (show f ++ "-")) $ mapMaybe (stripPrefix (b ++ "-")) l
+getCleanupList b f l = sort . map ((b ++ "-" ++ show f ++ "-") ++) $
+    mapMaybe (stripPrefix (show f ++ "-")) $
+    mapMaybe (stripPrefix (b ++ "-")) l
+  
 
 -- using System.Console.CmdArgs on account of System.Console.GetOpt being
 -- not unlike stabbed repeatedly in the face
 data TarsnapBackup = TarsnapBackup {
         frequency :: Frequency,
         dir :: FilePath,
-        cleanup :: Bool
+        cleanup :: Bool,
+        retain :: Int
         }
     deriving (Show, Data, Typeable)
 
 tb = TarsnapBackup {
-    frequency = Auto &= opt "Auto" &= help "Force a backup of type (Daily, Weekly, Monthly)",
-    cleanup = def &= help "Cleanup old backups",
+    frequency = Auto &= opt "Auto"
+        &= help "Force a backup of type (Daily, Weekly, Monthly)",
+    cleanup = True &= help "Cleanup old backups",
+    retain = 0 &= help "Number of backups to retain",
     dir = "" &= args &= typDir
     } &=
     help "This script manages Tarsnap backups" &=
@@ -89,12 +121,17 @@ main = do
     let cl = cleanup cmd
     let cl_type = whatCleanup (whichType (frequency cmd) d)
     rc <- doBackup (show (whichType (frequency cmd) d)) d (dir cmd)
+    let n = retain cmd
     case rc of
          ExitFailure e -> exitWith rc
          ExitSuccess -> if cl
                            then case cl_type of
                                     Nothing -> exitWith rc
-                                    Just f -> doCleanup (last (splitDirectories (dir cmd))) f >> exitWith rc
+                                    Just f -> doCleanup 
+                                                (last 
+                                                (splitDirectories (dir cmd))
+                                                ) 
+                                                f n >> exitWith rc
                         else exitWith rc
 
 whichType :: Frequency -> Day -> Frequency
