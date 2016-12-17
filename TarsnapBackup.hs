ba1dd4804591004048d91a3261563302831995d6
Andy Irving <irv@soundforsound.co.uk>
2016-12-17 19:07:31 +0000

    Formatting

--- TarsnapBackup.hs
+++ TarsnapBackup.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveDataTypeable, OverloadedStrings #-}
+
 {--
 
 Copyright (c) 2010, Andy Irving
@@ -28,6 +29,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 --}
 module TarsnapBackup where
+
 import Data.Time.Calendar
 import Data.Time.Calendar.OrdinalDate
 import Data.List
@@ -37,49 +39,54 @@ import System.FilePath
 import Data.Maybe
 import System.Exit
 
-data Frequency = Daily | Weekly | Monthly | Auto
-    deriving (Read, Show, Eq, Data, Typeable)
+data Frequency
+  = Daily
+  | Weekly
+  | Monthly
+  | Auto
+  deriving (Read, Show, Eq, Data, Typeable)
 
 doBackup :: String -> Day -> FilePath -> IO ExitCode
-doBackup f d b = readProcessWithExitCode
-                    "tarsnap" ["-c", "-f", archive_name b, b] [] >>= checkRes
-    where checkRes (rc, _, err) = case rc of
-                                            ExitFailure eno -> error $
-                                                "Unable to backup: (" ++
-                                                show eno ++ ")\n" ++ err
-                                            ExitSuccess -> return rc
-          basename d' =  last (splitDirectories d') -- /path/to/blah == blah
-           -- blah-Frequency-2010-09-11
-          archive_name b' = intercalate "-" [basename b', f, showGregorian d]
+doBackup f d b =
+  readProcessWithExitCode "tarsnap" ["-c", "-f", archive_name b, b] [] >>= checkRes
+  where
+    checkRes (rc, _, err) =
+      case rc of
+        ExitFailure eno ->
+          error $ "Unable to backup: (" ++ show eno ++ ")\n" ++ err
+        ExitSuccess -> return rc
+    basename d' = last (splitDirectories d') -- /path/to/blah == blah
+    -- blah-Frequency-2010-09-11
+    archive_name b' = intercalate "-" [basename b', f, showGregorian d]
 
 -- When doing the cleanup, remove backups which are of the lower frequency
 whatCleanup :: Frequency -> Maybe Frequency
 whatCleanup = whatType
-    where whatType Daily = Nothing
-          whatType Weekly = Just Daily
-          whatType Monthly = Just Weekly
-          whatType _ = Nothing
+  where
+    whatType Daily = Nothing
+    whatType Weekly = Just Daily
+    whatType Monthly = Just Weekly
+    whatType _ = Nothing
 
 doCleanup :: String -> Frequency -> Int -> IO ExitCode
-doCleanup b f n = readProcessWithExitCode
-                    "tarsnap" ["--list-archives"] [] >>= goCleanup
-    where goCleanup (rc, out, err) = case rc of
-                                       ExitFailure _ -> error $
-                                         "Unable to list archives for cleanup"
-                                           ++ "\n" ++ err
-                                       ExitSuccess -> case
-                                         getCleanupList b f (lines out) of
-                                           [] -> return rc
-                                           (xs) -> execCleanup
-                                                (drop n (reverse xs))
+doCleanup b f n = readProcessWithExitCode "tarsnap" ["--list-archives"] [] >>= goCleanup
+  where
+    goCleanup (rc, out, err) =
+      case rc of
+        ExitFailure _ ->
+          error $ "Unable to list archives for cleanup" ++ "\n" ++ err
+        ExitSuccess ->
+          case getCleanupList b f (lines out) of
+            [] -> return rc
+            (xs) -> execCleanup (drop n (reverse xs))
 
 execCleanup :: [String] -> IO ExitCode
 execCleanup l = do
-    (rc, _, err) <- readProcessWithExitCode
-        "tarsnap" (["-d", "-f"] ++ intersperse "-f" l) []
-    case rc of
-         ExitFailure _ ->  error err
-         ExitSuccess -> return rc
+  (rc, _, err) <-
+    readProcessWithExitCode "tarsnap" (["-d", "-f"] ++ intersperse "-f" l) []
+  case rc of
+    ExitFailure _ -> error err
+    ExitSuccess -> return rc
 
 -- from the input list:
 -- ["blah-Frequency-2010-09-11", "blah-Frequency-2010-09-12"]
@@ -87,41 +94,38 @@ execCleanup l = do
 -- then strip the "Frequency-" string if it's the frequency we're clearing up
 -- then remove any Nothing's from the list
 getCleanupList :: String -> Frequency -> [String] -> [String]
-getCleanupList b f l = sort . map ((b ++ "-" ++ show f ++ "-") ++) $
-    mapMaybe (stripPrefix (show f ++ "-")) $
-    mapMaybe (stripPrefix (b ++ "-")) l
-
+getCleanupList b f l =
+  sort . map ((b ++ "-" ++ show f ++ "-") ++) $
+  mapMaybe (stripPrefix (show f ++ "-")) $ mapMaybe (stripPrefix (b ++ "-")) l
 
 -- using System.Console.CmdArgs on account of System.Console.GetOpt being
 -- not unlike stabbed repeatedly in the face
-data TarsnapBackup = TarsnapBackup {
-        frequency :: Frequency,
-        dir :: FilePath,
-        cleanup :: Bool,
-        retain :: Int
-        }
-    deriving (Show, Data, Typeable)
+data TarsnapBackup = TarsnapBackup
+  { frequency :: Frequency
+  , dir :: FilePath
+  , cleanup :: Bool
+  , retain :: Int
+  } deriving (Show, Data, Typeable)
 
 tb :: TarsnapBackup
-tb = TarsnapBackup {
-    frequency = Auto &= opt "Auto"
-        &= help "Force a backup of type (Daily, Weekly, Monthly)",
-    cleanup = True &= help "Cleanup old backups",
-    retain = 0 &= help "Number of backups to retain",
-    dir = "" &= args &= typDir
-    } &=
-    help "This script manages Tarsnap backups" &=
-    summary "(c) Andy Irving <andy@soundforsound.co.uk> 2010"
-
-
+tb =
+  TarsnapBackup
+  { frequency =
+    Auto &= opt "Auto" &= help "Force a backup of type (Daily, Weekly, Monthly)"
+  , cleanup = True &= help "Cleanup old backups"
+  , retain = 0 &= help "Number of backups to retain"
+  , dir = "" &= args &= typDir
+  } &=
+  help "This script manages Tarsnap backups" &=
+  summary "(c) Andy Irving <andy@soundforsound.co.uk> 2010"
 
 whichType :: Frequency -> Day -> Frequency
 whichType f d = check f
-    where check Auto
-              | month_day == 1 = Monthly -- first day of the month
-              | week_day == 0 = Weekly   -- first day of the week
-              | otherwise = Daily
-          check _ = f
-          (_, _, month_day) = toGregorian d
-          (_, week_day) = sundayStartWeek d
-
+  where
+    check Auto
+      | month_day == 1 = Monthly -- first day of the month
+      | week_day == 0 = Weekly -- first day of the week
+      | otherwise = Daily
+    check _ = f
+    (_, _, month_day) = toGregorian d
+    (_, week_day) = sundayStartWeek d
--- main.hs
+++ main.hs
@@ -8,21 +8,19 @@ import System.FilePath
 
 main :: IO ExitCode
 main = do
-    cmd <- cmdArgsRun $ cmdArgsMode tb
-    time <- getCurrentTime
-    let d = utctDay time
-    let cl = cleanup cmd
-    let cl_type = whatCleanup (whichType (frequency cmd) d)
-    rc <- doBackup (show (whichType (frequency cmd) d)) d (dir cmd)
-    let n = retain cmd
-    case rc of
-         ExitFailure _ -> exitWith rc
-         ExitSuccess -> if cl
-                           then case cl_type of
-                                    Nothing -> exitWith rc
-                                    Just f -> doCleanup 
-                                                (last 
-                                                (splitDirectories (dir cmd))
-                                                ) 
-                                                f n >> exitWith rc
-                        else exitWith rc
+  cmd <- cmdArgsRun $ cmdArgsMode tb
+  time <- getCurrentTime
+  let d = utctDay time
+  let cl = cleanup cmd
+  let cl_type = whatCleanup (whichType (frequency cmd) d)
+  rc <- doBackup (show (whichType (frequency cmd) d)) d (dir cmd)
+  let n = retain cmd
+  case rc of
+    ExitFailure _ -> exitWith rc
+    ExitSuccess ->
+      if cl
+        then case cl_type of
+               Nothing -> exitWith rc
+               Just f ->
+                 doCleanup (last (splitDirectories (dir cmd))) f n >> exitWith rc
+        else exitWith rc
